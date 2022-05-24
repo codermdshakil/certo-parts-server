95f8cf077adaa533324f8bebc226a4ff4fb4fb7d
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-05-24 13:04:43 +0600

    single product get added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -43,6 +43,13 @@ async function run() {
             res.send(result);
         })
 
+        // get a single parts 
+        app.get('/parts/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const product = await partCollection.findOne(query);
+            res.send(product)
+        })
 
 
 
