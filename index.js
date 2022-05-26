5af6431c4855fb2b200470aedff683a19a4cb036
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-05-26 08:35:57 +0600

    verify jwt emplement added

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l4x92.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_SECRET_TOEKN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
+
 async function run() {
     try {
         await client.connect();
@@ -81,11 +99,17 @@ async function run() {
         })
 
         // get all order on email based 
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            const query = { email: email };
-            const result = await orderCollection.find(query).toArray();
-            res.send(result);
+            const decodedEmail = req.decoded.email;
+            if (decodedEmail == email) {
+                const query = { email: email };
+                const result = await orderCollection.find(query).toArray();
+                return res.send(result);
+            }
+            else{
+                return res.status(403).send({message:"Forbidden access"})
+            }
         })
 
         // delete a order 
@@ -120,14 +144,13 @@ async function run() {
                     userLinkedinUrl: userInfo.userLinkedinUrl,
                 }
             }
-
             const result = await userInformationCollection.updateOne(filter, updateDoc, options);
             res.send(result);
 
         })
 
         // get updated user 
-        app.get('/userInfo/:email', async (req, res) => {
+        app.get('/userInfo/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { userEmail: email };
             const result = await userInformationCollection.findOne(query);
