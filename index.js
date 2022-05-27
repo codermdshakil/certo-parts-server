85ce397f4896ada9888381af21384b60b30abf76
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-05-28 01:29:47 +0600

    delete method added

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ async function run() {
         })
 
         // delete product 
-        app.delete('/parts/:id',verifyJWT, async (req, res) => {
+        app.delete('/parts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await partCollection.deleteOne(query);
@@ -193,9 +193,17 @@ async function run() {
 
         })
 
+        // delete product 
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // delete a order 
-        app.delete('/orders/:id', verifyJWT, async (req, res) => {
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
